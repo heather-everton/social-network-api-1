adad5a52d0319667d308cdb9eefebb10bc5e4071
Jesse Parent <jesseparent@gmail.com>
2020-11-28 22:24:27 -0700

    add reaction

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -21,6 +21,21 @@ const thoughtController = {
       })
       .catch(err => res.json(err));
   },
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
 };
 
 module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
 const {
-  addThought
+  addThought,
+  addReaction
 } = require('../../controllers/thought-controller');
 
 // /api/thoughts/<userId>
 router.route('/:userId').post(addThought);
 
+// /api/thoughts/<userId>/<thoughtId>
+router
+  .route('/:userId/:thoughtId')
+  .put(addReaction)
 
 module.exports = router;
